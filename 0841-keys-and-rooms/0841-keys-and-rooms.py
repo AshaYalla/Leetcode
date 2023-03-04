a4edb2b560d6d5a81c8ebd8a6a2d3c629afeb4cd
Asha Yalla <asha18.yalla@gmail.com>
2023-03-04 12:48:09 -0800

    Time: 68 ms (67.01%), Space: 15.5 MB (13.39%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -4,19 +4,31 @@ class Solution:
         for i in range(len(rooms)):
             graph[i] += (rooms[i])
             
-        queue = graph[0]
-        visited = set()
-        visited.add(0)
+        seen = set()
+        seen.add(0)
         
-        while(queue):
-            i = queue.pop(0)
-            visited.add(i)
-            for child in graph[i]:
-                if child not in visited:
-                    queue.append(child)
-                    
-        print(visited, graph)
-        return(len(visited) == len(graph))
+        def dfs(room):
+            for i in graph[room]:
+                if i not in seen:
+                    seen.add(i)
+                    dfs(i)
+        dfs(0)
+            
+            
+        
+            
+#         queue = graph[0]
+#         visited = set()
+#         visited.add(0)
+        
+#         while(queue):
+#             i = queue.pop(0)
+#             visited.add(i)
+#             for child in graph[i]:
+#                 if child not in visited:
+#                     queue.append(child)
+        print(seen,graph)          
+        return(len(seen) == len(graph))
             
             
             
