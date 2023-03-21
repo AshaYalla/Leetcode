50e48b2075360cbb63d3fbed1086dd65dd9b5a17
Asha Yalla <asha18.yalla@gmail.com>
2023-03-20 22:25:53 -0700

    Time: 780 ms (78.68%), Space: 75.4 MB (64.54%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,46 +1,30 @@
 class Node:
-    def __init__(self,key,val):
-        self.key,self.val = key,val
-        self.next,self.prev = None,None
-    
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.next = self.prev = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.dictt = dict()
-        self.left, self.right = Node(0,0),Node(0,0)
-        self.left.next,self.right.prev = self.right, self.left
-        
-    def remove(self,node):
-        node.prev.next,node.next.prev = node.next,node.prev
-
-        
-    def insert(self,node):
-        prev,nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next,node.prev = nxt,prev
+        self.cache = collections.OrderedDict()
 
     def get(self, key: int) -> int:
-        if key in self.dictt:
-            self.remove(self.dictt[key])
-            self.insert(self.dictt[key])
-            return self.dictt[key].val
-        else:
-            return -1
+        if(key in self.cache):
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+        
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dictt:
-            self.remove(self.dictt[key])
-        self.dictt[key] = Node(key,value)  
-        self.insert(self.dictt[key])
-            
-        if len(self.dictt) > self.cap:
-            del self.dictt[self.left.next.key]
-            self.remove(self.left.next)
-                
-                
-            
+        if(key in self.cache):
+            self.cache.move_to_end(key)
+            self.cache[key] = value
+        else:
+            if len(self.cache) >= self.cap:
+                self.cache.popitem(last = False)
+            self.cache[key] = value
             
         
 
