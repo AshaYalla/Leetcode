7204d64a2724fc09936e019b2bce131e22fab275
Asha Yalla <asha18.yalla@gmail.com>
2023-04-07 21:04:33 -0700

    Time: 142 ms (28.59%), Space: 15 MB (21.93%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,21 +1,25 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
         visited = set()
-        r, c = len(grid)-1, len(grid[0]) -1 
-        def dfs(i,j):
-            if i < 0 or j < 0 or i > r or j > c:
-                return 0
-            if grid[i][j] == 1 or (i,j) in visited:
-                return 1
-            visited.add((i,j))
-            return( min( dfs(i+1,j), dfs(i-1,j), dfs(i,j-1), dfs(i,j+1)))
-            
-        res = 0  
-        for i in range(0,r):
-            for j in range(0,c):
-                if grid[i][j] == 0 and (i,j) not in visited:
-                    res+=dfs(i,j)
-        return res
-                    
+        queue = []
+        directions =[(0,1),(1,0),(-1,0),(0,-1)]
+        m, n = len(grid),len(grid[0])
+        answer = 0
+        for i in range(m):
+            for j in range(n):
+                if(grid[i][j]== 0 and (i,j) not in visited):
+                    boundaryflag = True
+                    queue.append((i,j))
+                    while(queue):
+                        ti,tj = queue.pop(0)
+                        visited.add((ti,tj))
+                        if ti in (0,m-1) or tj in (0,n-1):
+                            boundaryflag = False
+                        for ni,nj in directions:
+                            if(0<=ti+ni<m and 0<=tj+nj<n and grid[ti+ni][tj+nj] == 0 and (ti+ni,tj+nj) not in visited):
+                                queue.append((ti+ni,tj+nj))
+                                visited.add((ti+ni,tj+nj))
+                    if boundaryflag:
+                        answer+=1
+        return answer
                         
-                    
