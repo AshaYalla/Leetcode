6acd8e56ebc2d9cd29a22220fd895111cfbe85f6
Asha Yalla <asha18.yalla@gmail.com>
2023-06-13 18:32:54 -0700

    Time: 382 ms (27.45%), Space: 24.3 MB (20.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
         visited = set()
-        nei = [(-1,0),(1,0),(0,-1),(0,1),]
-        ans = 0
+        queue = []
+        m , n = len(grid), len(grid[0])
+        directions = [(-1,0),(1,0),(0,1),(0,-1)]
+        sum = 0
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        def bfs(start):
+            answer = 0
+            queue.append(start)
+            while queue:
+                qi,qj = queue.pop(0)
+                if((qi,qj) not in visited and grid[qi][qj] == "1" ):
+                    answer+=1
+                    visited.add(start)
+                for ii,jj in directions:
+                    if(0<=qi+ii<m and 0<=qj+jj<n and (qi+ii,qj+jj) not in visited and grid[qi+ii][qj+jj] == "1"):
+                        queue.append((qi+ii,qj+jj))
+                        visited.add((qi+ii,qj+jj))
+            return answer
+        for i in range(m):
+            for j in range(n):
+                if((i,j) not in visited and grid[i][j] == "1" ):
+                    sum += bfs((i,j))
+        return sum
+    
                 
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    ans+=1
-                    queue = [(i,j)]
-                    visited.add((i,j))
-                    while(queue):
-                        a,b =  queue.pop()
-                        
-                        for l,m in nei:
-                            if (a+l >=0 and a+l <len(grid)) and (b+m >=0 and b+m <len(grid[0])) :
-                                if (grid[a+l][b+m] == "1" ) and (a+l,b+m) not in visited:
-                                    queue.append((a+l,b+m))    
-                                    visited.add((a+l,b+m))
-                                
-        return ans
-
+        
         
