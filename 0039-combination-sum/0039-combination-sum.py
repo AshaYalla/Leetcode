b170e470b992ccf0f736c126f602dc86e524edec
Asha Yalla <asha18.yalla@gmail.com>
2023-06-26 19:25:43 -0700

    Time: 72 ms (76.07%), Space: 16.5 MB (22.45%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,35 +1,16 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
-        n = len(candidates) - 1
-        
-        def combi(i,target,arr):
-            if(i == n):
-                if(target == 0):
-                    ans.append(arr)
+        def combi(tar, comb, start):
+            if tar == 0:
+                ans.append(list(comb))
                 return
-            if candidates[i] <= target:
-                arr.append(candidates[i])
-                combi(i,target-candidates[i],arr)
-                arr.pop()
-            combi(i+1,target,arr)
-        combi(0,target,[])
+            elif tar < 0:
+                return 
+            for i in range(start,len(candidates)):
+                comb.append(candidates[i])
+                combi(tar - candidates[i],comb,i)
+                comb.pop()
+        combi(target,[],0)
         return ans
-    
-class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        
-        def findcombination(i,target,li,ans):
-            if i==len(candidates): # Base Condition
-                if target==0 and li not in ans:
-                    ans.append(li.copy())
-                return     
-            if candidates[i]<=target:
-                li.append(candidates[i])
-                findcombination(i,target-candidates[i],li,ans)
-                li.pop()
-            findcombination(i+1,target,li,ans)
-            return ans
-            
-        return findcombination(0,target,li=[],ans=[]) 
             
