5609f8ffc1b96d896f519f6006bf70ac933e3399
Asha Yalla <asha18.yalla@gmail.com>
2023-07-04 10:10:27 -0700

    Time: 44 ms (79.72%), Space: 16.3 MB (75.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,19 +1,35 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        summ = 0
-        n = len(nums)
-        dictt = {}
-        def solve(i):
-            if i >= n:
-                return 0
-            if i in dictt:
-                return dictt[i]
-            c1 = nums[i] + solve(i+2)
-            c2 = solve(i+1)
-            summ = max(c1,c2)
-            dictt[i] = summ
-            return summ
-        return solve(0)
+        prev = nums[0]
+        prev2 = 0
+        
+        for i in range(1,len(nums)):
+            cur = prev2 + nums[i]
+            ans = max(cur,prev)
+            prev2 = prev
+            prev = ans
+            
+        return prev
+            
+        
+        
+        
+        
+        
+        # summ = 0
+        # n = len(nums)
+        # dictt = {}
+        # def solve(i):
+        #     if i >= n:
+        #         return 0
+        #     if i in dictt:
+        #         return dictt[i]
+        #     c1 = nums[i] + solve(i+2)
+        #     c2 = solve(i+1)
+        #     summ = max(c1,c2)
+        #     dictt[i] = summ
+        #     return summ
+        # return solve(0)
         
             
             
