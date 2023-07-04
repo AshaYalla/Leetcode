2e1c214b3b06a86136e3d6350eb6bf95d8711b93
Asha Yalla <asha18.yalla@gmail.com>
2023-07-04 07:26:41 -0700

    Time: 47 ms (50.91%), Space: 16.2 MB (67.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,18 +1,40 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dictt = {}
-        def solve(i):
-            if i == 0:
-                return 1
-            if i < 0:
-                return 0 
-            if i in dictt:
-                return dictt[i]
-            c1 = solve(i-2)
-            c2 = solve(i-1)
-            dictt[i] = c1+c2
-            return c1+c2
-        return solve(n)
+
+        prev = 0 
+        curr = 1
+        ans = 0
+        for i in range(1,n+1):
+            ans = prev + curr
+            prev = curr
+            curr = ans
+            
+        return ans
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         dictt = {}
+#         def solve(i):
+#             if i == 0:
+#                 return 1
+#             if i < 0:
+#                 return 0 
+#             if i in dictt:
+#                 return dictt[i]
+#             c1 = solve(i-2)
+#             c2 = solve(i-1)
+#             dictt[i] = c1+c2
+#             return c1+c2
+#         return solve(n)
         
             
         
