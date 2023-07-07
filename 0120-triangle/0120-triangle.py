969c35a3efe2f12138c806b8673a97ec74213dfa
Asha Yalla <asha18.yalla@gmail.com>
2023-07-06 19:46:30 -0700

    Time: 100 ms (6.17%), Space: 19.9 MB (6.86%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        a = len(triangle)
+        b = len(triangle[-1])
+        dp = dict()
+        def solve(i,j):
+            if  i == 0:
+                return triangle[0][0]
+            if (i,j) in dp:
+                return dp[(i,j)]
+            if j > i or j < 0:
+                return inf
+            dp[(i,j)] = triangle[i][j] + min(solve(i-1,j), solve(i-1,j-1))
+            return dp[(i,j)]
+        
+        minn = inf
+        
+        for j in range(b):
+            minn = min(minn, solve(a-1,j))
+        return minn
+            
+            
+            
+            
+            
+            # if i == m:
+            #     return min(triangle[i][j], triangle[i][j+1] if j < n -1 else 0)
+            # if j > i+1:
+            #     return 0
+            # triangle
+            
+            
