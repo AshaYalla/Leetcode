516e80b6f0adf040f2f30db9fcd06bf7aef0e8c9
Asha Yalla <asha18.yalla@gmail.com>
2023-07-13 06:45:32 -0700

    Time: 336 ms (60.33%), Space: 38 MB (14.58%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -19,5 +19,59 @@ class Solution:
             
         return summ(0,0)
     
+    
+#     	arr.sort()
+# 	    dp = dict()
+# 	    mod=10**9+7
+		
+# 		#recursion
+#         def recursion(i,k):
+#             if i == n:
+#                 if k == 0:
+#                     return 1
+#                 else:
+#                     return 0
+#             if (i,k) in dp:
+#                 return dp[(i,k)]
+            
+#             summ =  recursion(i+1,k)
+#             if arr[i] <= k:
+#                 summ += recursion(i+1,k-arr[i])
+#             dp[(i,k)] = summ 
+#             return summ
+#         return recursion(0,sum) %mod
+        
+#         #tabulation
+    
+#         dp = [[0 for i in range(sum+1)] for j in range(n+1)]
+        
+#         for i in range(n+1):
+#             dp[i][0] = 1
+        
+#         for i in range(1,n+1):
+#             for j in range(0,sum+1):
+#                 if j < arr[i-1]:
+#                     dp[i][j] = dp[i-1][j]
+#                 else:
+#                     dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]]
+#         return dp[-1][-1]%1000000007
+        
+#         #tabulation Optimized
+        
+#         prev = [0 for i in range(sum+1)] 
+        
+#         for i in range(n+1):
+#             prev[0] = 1
+        
+#         for i in range(1,n+1):
+#             curr = [0 for i in range(sum+1)] 
+#             for j in range(0,sum+1):
+#                 if j < arr[i-1]:
+#                     curr[j] = prev[j]
+#                 else:
+#                     curr[j] = prev[j] + prev[j-arr[i-1]]
+#             prev = curr
+#         return prev[-1]%1000000007
+    
 
         
