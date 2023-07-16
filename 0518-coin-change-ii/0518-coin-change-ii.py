0059879e3d8c06b18ebc1c550d6fdabc7dbdaeb2
Asha Yalla <asha18.yalla@gmail.com>
2023-07-16 07:48:16 -0700

    Time: 233 ms (69.83%), Space: 16.6 MB (73.87%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,22 +1,41 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
-        dp = {}
-        def solve(i,rem):
-            if (i,rem) in dp:
-                return dp[(i,rem)]
-            if rem < 0:
-                return 0
-            if rem == 0:
-                return 1
-            if i < 0:
-                if rem == 0:
-                    return 1
+        dp = [0 for i in range(amount+1)]
+        dp[0] = 1
+        
+        for i in range(n):
+            for j in range(amount+1):
+                if j < coins[i]:
+                    continue
                 else:
-                    return 0
+                    dp[j] = dp[j-coins[i]] + dp[j]
+        return dp[-1]
+        
+        
+                
+                
+                
+                
+                
+                
+                
+#         dp = {}
+#         def solve(i,rem):
+#             if (i,rem) in dp:
+#                 return dp[(i,rem)]
+#             if rem < 0:
+#                 return 0
+#             if rem == 0:
+#                 return 1
+#             if i < 0:
+#                 if rem == 0:
+#                     return 1
+#                 else:
+#                     return 0
             
-            ans = solve(i,rem-coins[i]) + solve(i-1,rem)
-            dp[(i,rem)] = ans
-            return ans
-        return solve(n-1,amount)
+#             ans = solve(i,rem-coins[i]) + solve(i-1,rem)
+#             dp[(i,rem)] = ans
+#             return ans
+#         return solve(n-1,amount)
         
