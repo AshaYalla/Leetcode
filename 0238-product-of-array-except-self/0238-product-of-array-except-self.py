11937e633440b4eb94e837dcb50dff97a0528161
Asha Yalla <asha18.yalla@gmail.com>
2023-08-08 11:53:58 -0700

    Time: 201 ms (88.67%), Space: 24.3 MB (38.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,13 +3,13 @@ class Solution:
         ans = [1] * len(nums)
         for i in range(1,len(nums)):
             ans[i] = ans[i-1] * nums[i-1]
-        print(ans)
         prev = 1
-        for j in range(len(nums) - 2, -1,-1):
-            print(prev)
-            prev = nums[j+1] * prev
-            ans[j] = ans[j] * prev
+        for i in range(len(nums) -2, -1, -1):
+            prev = prev * nums[i+1]
+            ans[i] = ans[i] * prev
+            
         return ans
+           
             
             
             
