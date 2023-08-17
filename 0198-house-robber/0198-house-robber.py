94df868d2e0cbd32d06bd0d12f166e96932556d5
Asha Yalla <asha18.yalla@gmail.com>
2023-08-17 11:02:16 -0700

    Time: 46 ms (46.98%), Space: 16.3 MB (72.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,11 +1,21 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        @cache
-        def solve(i):
-            if i >= n:
-                return 0
-            maxx = max(solve(i+2) + nums[i] , solve(i+1))
-            return maxx
-        return solve(0)
+        # @cache
+        # def solve(i):
+        #     if i >= n:
+        #         return 0
+        #     maxx = max(solve(i+2) + nums[i] , solve(i+1))
+        #     return maxx
+        # return solve(0)
+        
+        if(n == 1):
+            return nums[0]
+        
+        dp = [0] * (n)
+        dp[0] = nums[0]
+        dp[1] = max(nums[1],nums[0])
+        for i in range(2,n):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        return dp[n-1]
         
