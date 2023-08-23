144f4a23d760b7e6015726e257a4919e931eeba1
Asha Yalla <asha18.yalla@gmail.com>
2023-08-23 08:25:13 -0700

    Time: 302 ms (71.69%), Space: 18.9 MB (66.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        rl = len(grid)
+        cl = len(grid[0])
         
-        visited = set()
-        nei = [(-1,0),(1,0),(0,-1),(0,1),]
-        ans = 0
+        def dfs(r,c):
+            grid[r][c]  = '0'
+            nei = [(1,0),(0,1),(-1,0),(0,-1)]
+            
+            for i,j in nei:
+                if r+i < rl and r+i >= 0 and c+j < cl and c+j >=0 and grid[r+i][c+j] == '1':
+                    dfs(r+i,c+j)
+            
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    ans+=1
-                    queue = [(i,j)]
-                    visited.add((i,j))
-                    while(queue):
-                        a,b =  queue.pop()
-                        
-                        for l,m in nei:
-                            if (a+l >=0 and a+l <len(grid)) and (b+m >=0 and b+m <len(grid[0])) :
-                                if (grid[a+l][b+m] == "1" ) and (a+l,b+m) not in visited:
-                                    queue.append((a+l,b+m))    
-                                    visited.add((a+l,b+m))
-                                
-        return ans
-
+        iland = 0
+        
+        for i in range(rl):
+            for j in range(cl):
+                if grid[i][j] == '1':
+                    iland +=1
+                    dfs(i,j)
+        return iland
+                    
         
