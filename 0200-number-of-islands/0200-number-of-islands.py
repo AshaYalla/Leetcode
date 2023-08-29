4900dbeea8d821f016648024739c61e56923e7a9
Asha Yalla <asha18.yalla@gmail.com>
2023-08-29 08:29:06 -0700

    Time: 311 ms (60.56%), Space: 28.4 MB (5.11%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,46 +1,51 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
     
+#         rl = len(grid)
+#         cl = len(grid[0])
+#         visited = set()
+#         iland = 0
+#         queue = []
+#         for i in range(rl):
+#             for j in range(cl):
+#                 if grid[i][j] == '1' and (i,j) not in visited:
+#                     iland +=1
+#                     queue = [(i,j)]
+#                     visited.add((i,j))
+                
+#                 while(queue):
+#                     r,c = queue.pop()
+#                     nei = [(1,0),(0,1),(-1,0),(0,-1)]
+#                     for a,b in nei:
+#                         if r+a < rl and r+a >= 0 and c+b < cl and c+b >=0 and grid[r+a][c+b] == '1' and (r+a,c+b) not in visited:
+#                             queue.append((r+a,c+b))
+#                             visited.add((r+a,c+b))
+                            
+#         return iland
+       
         rl = len(grid)
         cl = len(grid[0])
         visited = set()
+        
+        def dfs(r,c):
+            
+            
+            nei = [(1,0),(0,1),(-1,0),(0,-1)]
+            
+            for i,j in nei:
+                if r+i < rl and r+i >= 0 and c+j < cl and c+j >=0 and grid[r+i][c+j] == '1' and (r+i,c+j) not in visited:
+                    visited.add((r+i,c+j))
+                    dfs(r+i,c+j)
         iland = 0
-        queue = []
+        
         for i in range(rl):
             for j in range(cl):
                 if grid[i][j] == '1' and (i,j) not in visited:
-                    iland +=1
-                    queue = [(i,j)]
+
                     visited.add((i,j))
-                
-                while(queue):
-                    r,c = queue.pop()
-                    nei = [(1,0),(0,1),(-1,0),(0,-1)]
-                    for a,b in nei:
-                        if r+a < rl and r+a >= 0 and c+b < cl and c+b >=0 and grid[r+a][c+b] == '1' and (r+a,c+b) not in visited:
-                            queue.append((r+a,c+b))
-                            visited.add((r+a,c+b))
-                            
+                    iland +=1
+                    dfs(i,j)
         return iland
-       
-        # rl = len(grid)
-#         cl = len(grid[0])
-        
-#         def dfs(r,c):
-#             grid[r][c]  = '0'
-#             nei = [(1,0),(0,1),(-1,0),(0,-1)]
-            
-#             for i,j in nei:
-#                 if r+i < rl and r+i >= 0 and c+j < cl and c+j >=0 and grid[r+i][c+j] == '1':
-#                     dfs(r+i,c+j)
-#         iland = 0
-        
-#         for i in range(rl):
-#             for j in range(cl):
-#                 if grid[i][j] == '1':
-#                     iland +=1
-#                     dfs(i,j)
-#         return iland
     
                     
                     
