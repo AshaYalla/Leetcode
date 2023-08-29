0d60f5d20769ef19a0e6d78174fe366fb828afdb
Asha Yalla <asha18.yalla@gmail.com>
2023-08-29 08:58:55 -0700

    Time: 210 ms (72.41%), Space: 18.5 MB (9.17%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -7,21 +7,49 @@ class Solution:
                     adj[i].add(j)
                     adj[j].add(i)
                     
-        queue = []    
+        def dfs(node):
+            for j in adj[node]:
+                if j not in visited:
+                    visited.add(j)
+                    dfs(j)
+                    
+ 
         visited = set()
         prov = 0
         
         for i in adj:
             if i not in visited:
                 visited.add(i)
-                queue.append(i)
                 prov+=1
-            while(queue):
-                node = queue.pop()
-                for j in adj[node]:
-                    if j not in visited:
-                        visited.add(j)
-                        queue.append(j)
-                        
+                dfs(i)
         return len(isConnected) - len(visited) + prov
+
             
+        
+        
+        
+        
+#         adj = defaultdict(set)
+#         for i in range(len(isConnected)):
+#             for j in range(len(isConnected[0])):
+#                 if isConnected[i][j] == 1 and i!=j:
+#                     adj[i].add(j)
+#                     adj[j].add(i)
+                    
+#         queue = []    
+#         visited = set()
+#         prov = 0
+        
+#         for i in adj:
+#             if i not in visited:
+#                 visited.add(i)
+#                 queue.append(i)
+#                 prov+=1
+#             while(queue):
+#                 node = queue.pop()
+#                 for j in adj[node]:
+#                     if j not in visited:
+#                         visited.add(j)
+#                         queue.append(j)
+                        
+#         return len(isConnected) - len(visited) + prov
