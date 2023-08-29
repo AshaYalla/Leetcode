e62d6fd8f727ec5778195525cae563ad3b30ce1f
Asha Yalla <asha18.yalla@gmail.com>
2023-08-29 08:48:38 -0700

    Time: 205 ms (83.32%), Space: 17.4 MB (38.12%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,32 +1,27 @@
 class Solution:
-    def __init__(self):
-        self.cities_visited = set()
-
-    def bfs(self, city, cities_graph):
-        self.cities_visited.add(city)
-        queue = [city]
-        while queue:
-            c = queue.pop(0)
-            for i in cities_graph[c]:
-                if i not in self.cities_visited:
-                    self.cities_visited.add(i)
-                    queue.append(i)
-
-    def findCircleNum(self, isConnected: list[list[int]]) -> int:
-        rows, cols = len(isConnected), len(isConnected[0])
-
-        cities_graph = [[] for _ in range(rows)]
-
-        for i in range(rows):
-            for j in range(cols):
-                if i != j and isConnected[i][j] == 1:
-                    cities_graph[i].append(j)
-
-        provinces = 0
-
-        for city in range(rows):
-            if city not in self.cities_visited:
-                self.bfs(city, cities_graph)
-                provinces += 1
-
-        return provinces
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        adj = defaultdict(set)
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected[0])):
+                if isConnected[i][j] == 1 and i!=j:
+                    adj[i].add(j)
+                    adj[j].add(i)
+                    
+        queue = []    
+        visited = set()
+        prov = 0
+        
+        for i in adj:
+            if i not in visited:
+                visited.add(i)
+                queue.append(i)
+                prov+=1
+            while(queue):
+                node = queue.pop()
+                for j in adj[node]:
+                    if j not in visited:
+                        visited.add(j)
+                        queue.append(j)
+                        
+        return len(isConnected) - len(visited) + prov
+            
