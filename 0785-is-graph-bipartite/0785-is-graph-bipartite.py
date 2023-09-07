4b054c27bd3b14774e995705adfa2663b88ca299
Asha Yalla <asha18.yalla@gmail.com>
2023-09-07 11:27:01 -0700

    Time: 171 ms (42.07%), Space: 17.1 MB (15.23%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,22 +1,43 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         color = [-1 for i in range(len(graph))]
-        queue = [0]
+        
+        def dfs(node):
+            for nei in graph[node]:
+                if color[nei] ==  color[node]:
+                        return False
+                if color[nei] == -1:
+                    color[nei] = not color[node]
+                    if not dfs(nei) :
+                        return False
+            return True
         
         for i in range(len(graph)):
             if color[i] == -1:
-                queue.append(i)
                 color[i] = True
-
-                while(queue):
-                    node = queue.pop(0)
-                        
-                    for nei in graph[node]:
-                        if color[nei] ==  color[node]:
-                                return False
-                        if color[nei] == -1:
-                            color[nei] = not color[node]
-                            queue.append(nei)
+                if not dfs(i):
+                    return False
         return True
+
+        
+        
+        
+#         queue = [0]
+        
+#         for i in range(len(graph)):
+#             if color[i] == -1:
+#                 queue.append(i)
+#                 color[i] = True
+
+#                 while(queue):
+#                     node = queue.pop(0)
+                        
+#                     for nei in graph[node]:
+#                         if color[nei] ==  color[node]:
+#                                 return False
+#                         if color[nei] == -1:
+#                             color[nei] = not color[node]
+#                             queue.append(nei)
+#         return True
 
             
