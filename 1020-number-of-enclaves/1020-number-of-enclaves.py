c732213ea0bb2ca5f19c5e1542548586d1a322df
Asha Yalla <asha18.yalla@gmail.com>
2023-09-06 22:13:49 -0700

    Time: 801 ms (15.16%), Space: 32.8 MB (70.04%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,6 +1,6 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        r,c = len(grid)-1, len(grid[0])-1
+        r,c = len(grid), len(grid[0])
         visited = set()
         nei = [(0,1),(1,0),(0,-1),(-1,0)]
         res = 0
@@ -11,11 +11,11 @@ class Solution:
                 ans = 0
                 while(queue):
                     ti, tj = queue.pop()
-                    if (ti,tj) not in visited  and ti >=0 and ti <=r and tj >=0 and tj <=c  and grid[ti][tj] == 1:
+                    if (ti,tj) not in visited  and ti >=0 and ti <r and tj >=0 and tj <c  and grid[ti][tj] == 1:
                         ans+=1
                         visited.add((ti,tj))
                         queue.extend([(ti+1,tj),(ti,tj+1),(ti-1,tj),(ti,tj-1)])
-                        if ti  in (0,r) or tj  in(0,c):
+                        if ti  in (0,r-1) or tj  in(0,c-1):
                             con = False
                 if con:
                     res+=ans
