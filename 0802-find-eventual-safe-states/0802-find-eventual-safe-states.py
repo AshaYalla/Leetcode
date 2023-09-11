b5063d458e68e25946115e8475d77faed6f059f4
Asha Yalla <asha18.yalla@gmail.com>
2023-09-10 18:15:46 -0700

    Time: 579 ms (93.09%), Space: 24.8 MB (10.70%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,26 +1,123 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-            
-        visited = set()
-        path = set()
-        safe = set()
         
-        def dfs(course):
-            for nei in graph[course]:
-                if nei in path:
+        visited = set()
+        pathvis = set()
+        res = [0]* len(graph)
+        
+        def dfs(i):
+            
+            if i in pathvis:
+                return False
+            if i in visited:
+                return True
+            visited.add(i)
+            pathvis.add(i)
+            for nei in graph[i]:
+                if not dfs(nei):
                     return False
-                    
-                if nei not in visited:
-                    visited.add(nei)
-                    path.add(nei)
-                    if not dfs(nei):
-                        return False
-                    path.remove(nei)
-            safe.add(course)        
+            pathvis.remove(i)    
+            res[i] = 1
             return True
         
         for i in range(len(graph)):
-            if i not in visited:
-                dfs(i)
-        return sorted(list(safe))
+            dfs(i)
+        ans = []
+        print(res)
+        for i in range(len(res)):
+            if res[i] == 1:
+                ans.append(i)
+        return ans
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         n = len(graph)
+#         indegree = [0] * n
+        
+#         for i in range(n):
+#             for node in graph[i]:
+#                 indegree[i]+=1
+                
+#         q = deque()
+#         for i in range(n):
+#             if indegree[i] == 0:
+#                 q.append(i)
+                
+#         safe = []
+#         print(q)
+        
+#         while(q):
+#             node = q.popleft()
+#             safe.append(node)
+#             for nei in graph[node]:
+#                 indegree[nei] -=1
+#                 if indegree[nei] ==0:
+#                     q.append(nei)
+                    
+#         return sorted(safe)
+                    
+                    
+            
+        
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            
+#         visited = set()
+#         path = set()
+#         safe = set()
+        
+#         def dfs(course):
+#             for nei in graph[course]:
+#                 if nei in path:
+#                     return False
+                    
+#                 if nei not in visited:
+#                     visited.add(nei)
+#                     path.add(nei)
+#                     if not dfs(nei):
+#                         return False
+#                     path.remove(nei)
+#             safe.add(course)        
+#             return True
+        
+#         for i in range(len(graph)):
+#             if i not in visited:
+#                 dfs(i)
+#         return sorted(list(safe))
         
