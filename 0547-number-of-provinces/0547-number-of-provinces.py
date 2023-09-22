05cd96f3d574ad6cf210e9716fa3baf82f061b6d
Asha Yalla <asha18.yalla@gmail.com>
2023-09-21 17:19:43 -0700

    Time: 213 ms (43.41%), Space: 17 MB (94.39%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,28 +1,54 @@
-class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        adj = defaultdict(set)
-        for i in range(len(isConnected)):
-            for j in range(len(isConnected[0])):
-                if isConnected[i][j] == 1 and i!=j:
-                    adj[i].add(j)
-                    adj[j].add(i)
+class Solution(object):
+    def findCircleNum(self, isConnected):
+
+        n = len(isConnected)
+        parent = [i for i in range(n)]
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            fa, fb = find(x), find(y)
+            parent[fb] = fa
+
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j]:
+                    union(i, j)
+        count = 0
+        for i in range(n):
+            if parent[i] == i:
+                count+=1
+        return count
+
+
+# class Solution:
+#     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+#         adj = defaultdict(set)
+#         for i in range(len(isConnected)):
+#             for j in range(len(isConnected[0])):
+#                 if isConnected[i][j] == 1 and i!=j:
+#                     adj[i].add(j)
+#                     adj[j].add(i)
                     
-        def dfs(node):
-            for j in adj[node]:
-                if j not in visited:
-                    visited.add(j)
-                    dfs(j)
+#         def dfs(node):
+#             for j in adj[node]:
+#                 if j not in visited:
+#                     visited.add(j)
+#                     dfs(j)
                     
  
-        visited = set()
-        prov = 0
+#         visited = set()
+#         prov = 0
         
-        for i in adj:
-            if i not in visited:
-                visited.add(i)
-                prov+=1
-                dfs(i)
-        return len(isConnected) - len(visited) + prov
+#         for i in adj:
+#             if i not in visited:
+#                 visited.add(i)
+#                 prov+=1
+#                 dfs(i)
+#         return len(isConnected) - len(visited) + prov
 
             
         
