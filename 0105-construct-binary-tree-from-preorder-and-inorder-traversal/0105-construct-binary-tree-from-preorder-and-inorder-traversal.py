c34329e1ff147aee92a72f5723ddbc333269c91e
Asha Yalla <asha18.yalla@gmail.com>
2023-11-06 10:36:57 -0800

    Time: 126 ms (71.47%), Space: 55.4 MB (64.19%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,30 +1,16 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        # preorder = Root L R
-        # inorder = L Root R
-        if not preorder:
+        # print(preorder, inorder)
+        if len(inorder) == 0: #1
             return None
-        val = preorder.pop(0)
-        idx = inorder.index(val)
-        root = TreeNode(val)
-        stack = [
-            (root, 'left', preorder[:idx], inorder[:idx]),
-            (root, 'right', preorder[idx:], inorder[idx+1:])]
-        while stack:
-            node, direction, preorder, inorder = stack.pop(0)
-            if not preorder:
-                continue
-            val = preorder.pop(0)
-            idx = inorder.index(val)
-            setattr(node, direction, TreeNode(val))
-            stack.extend([
-                (getattr(node, direction), 'left', preorder[:idx], inorder[:idx]),
-                (getattr(node, direction), 'right', preorder[idx:], inorder[idx+1:])])
-        return root
         
+        if len(preorder) == 1: #2
+            return TreeNode(preorder[0])
+        
+        ind = inorder.index(preorder.pop(0)) #3
+        node = TreeNode(inorder[ind])
+        
+        node.left = self.buildTree(preorder, inorder[:ind]) #4
+        node.right = self.buildTree(preorder, inorder[ind+1:]) #5
+     
+        return node
