0dd1429f522c63d1da30b24634871cb44aa7b2e0
Asha Yalla <asha18.yalla@gmail.com>
2023-11-13 08:54:51 -0800

    Time: 43 ms (20.57%), Space: 16 MB (99.76%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -4,21 +4,78 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
+        ans = []
+        cur = root
+        while(cur!=None):
+            if cur.left == None:
+                ans.append(cur.val)
+                cur = cur.right
+            else:
+                node = cur.left 
+                while(node.right and node.right != cur):
+                    node = node.right
+                if node.right == None:
+                    node.right = cur
+                    cur = cur.left
+                else:
+                    node.right = None
+                    ans.append(cur.val)
+                    cur = cur.right
+        return ans
         
-        def dfs(x):
-            if x:
-                dfs(x.left)
-                res.append(x.val)
-                dfs(x.right)
+        
+        
+#         ans = []
+#         cur = root
+#         while cur != None:
+#             #printing the leftmost node
+#             if not cur.left:
+#                 ans.append(cur.val)
+#                 cur = cur.right
+#             else:
+#                 temp = cur
+#                 temp = temp.left
+#                 #going to the rightmost node in the left subtree (lets call it temp)
+#                 while temp.right and temp.right != cur:
+#                     temp = temp.right
+                
+#                 #2 conditions arise:
+                
+#                 #i. the right child of temp doesn't exist (The thread to the cur node has not been made)
+#                 #in this case, point the right child of temp to cur and move cur to its left child
+#                 if not temp.right:
+#                     temp.right = cur
+#                     cur = cur.left
+
+#                 #ii. the thread has already been created so we break the thread
+#                 #(pointing the temp's right child back to None)and print cur.
+#                 #Finally, move cur to its right child      
+#                 else:
+#                     ans.append(cur.val)
+#                     temp.right = None
+#                     cur = cur.right
+
+#         return ans
+
+
+# class Solution:
+#     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         res = []
+        
+#         def dfs(x):
+#             if x:
+#                 dfs(x.left)
+#                 res.append(x.val)
+#                 dfs(x.right)
                 
                 
             
             
-        dfs(root)
-        return res
+#         dfs(root)
+#         return res
             
 #         res = []
 #         stack = []
