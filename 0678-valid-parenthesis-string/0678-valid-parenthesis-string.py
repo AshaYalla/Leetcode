ebd49d1a21a46ebd4c1511ed3c2706f2236d9902
Asha Yalla <asha18.yalla@gmail.com>
2024-02-09 09:26:14 -0800

    Time: 34 ms (74.95%), Space: 16.4 MB (85.78%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -41,43 +41,65 @@
 
 #         return True
 
+# class Solution:
+#     def checkValidString(self, s: str) -> bool:
+        
+#         # store the indices of '('
+#         stk = []
+        
+#         # store the indices of '*'
+#         star = []
+        
+        
+#         for idx, char in enumerate(s):
+            
+#             if char == '(':
+#                 stk.append( idx )
+                
+#             elif char == ')':
+                
+#                 if stk:
+#                     stk.pop()
+#                 elif star:
+#                     star.pop()
+#                 else:
+#                     return False
+            
+#             else:
+#                 star.append( idx )
+        
+        
+#         # cancel ( and * with valid positions, i.e., '(' must be on the left hand side of '*'
+#         while stk and star:
+#             if stk[-1] > star[-1]:
+#                 return False
+        
+#             stk.pop()
+#             star.pop()
+        
+        
+#         # Accept when stack is empty, which means all braces are paired
+#         # Reject, otherwise.
+#         return len(stk) == 0
+
+
+
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        
-        # store the indices of '('
-        stk = []
-        
-        # store the indices of '*'
-        star = []
-        
-        
-        for idx, char in enumerate(s):
-            
-            if char == '(':
-                stk.append( idx )
-                
-            elif char == ')':
-                
-                if stk:
-                    stk.pop()
-                elif star:
-                    star.pop()
-                else:
-                    return False
-            
+        leftmax, leftmin = 0,0
+        for i in s:
+            if i == '(':
+                leftmax +=1
+                leftmin +=1
+            elif i == ')':
+                leftmax -=1
+                leftmin = max(leftmin-1,0)
             else:
-                star.append( idx )
-        
-        
-        # cancel ( and * with valid positions, i.e., '(' must be on the left hand side of '*'
-        while stk and star:
-            if stk[-1] > star[-1]:
+                leftmax +=1
+                leftmin = max(leftmin-1,0)
+            if leftmax < 0:
                 return False
-        
-            stk.pop()
-            star.pop()
-        
-        
-        # Accept when stack is empty, which means all braces are paired
-        # Reject, otherwise.
-        return len(stk) == 0
+        return leftmin == 0
+                
+                
+                
