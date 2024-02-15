320969f8ba7d4460379d9b27f862d12dc79ec191
Asha Yalla <asha18.yalla@gmail.com>
2024-02-15 14:35:46 -0800

    Time: 32 ms (85.04%), Space: 16.7 MB (52.20%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,21 +6,67 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root == None:
+            return []
+        ans = []
+        def dfs(curr):
+            nextt = []
+            for x in curr:
+                if x:
+                    if x.left:
+                        nextt.append(x.left)
+                    if x.right:
+                        nextt.append(x.right)
+                    right = x.val
+            ans.append(right)
+
+            if nextt:
+                dfs(nextt)
+        dfs([root])
+        return ans
         
-        q = [root]
-        res = []
-        while(q):
-            ans = []
-            for i in range(len(q)):
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         q = [root]
+#         res = []
+#         while(q):
+#             ans = []
+#             for i in range(len(q)):
                 
-                ele = q.pop(0)
-                if(ele):
-                    ans.append(ele.val)
-                    q.append(ele.left)
-                    q.append(ele.right)
-            if ans:
-                res.append(ans[-1])
-        return res
+#                 ele = q.pop(0)
+#                 if(ele):
+#                     ans.append(ele.val)
+#                     q.append(ele.left)
+#                     q.append(ele.right)
+#             if ans:
+#                 res.append(ans[-1])
+#         return res
                     
                     
                 
