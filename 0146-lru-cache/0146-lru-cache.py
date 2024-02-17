9369a399d929cce65d56adc836e04d10b3694256
Asha Yalla <asha18.yalla@gmail.com>
2024-02-17 07:33:42 -0800

    Time: 566 ms (69.33%), Space: 77.9 MB (70.64%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,31 +1,59 @@
-class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.next = self.prev = None
-
 class LRUCache:
+    class Node:
+        def __init__(self, key, val):
+            self.key = key
+            self.val = val
+            self.prev, self.nxt = None, None
 
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = collections.OrderedDict()
+        self.capacity = capacity
+        self.dictt = dict()
+        self.head = self.Node(-1,-1)
+        self.tail = self.Node(-1,-1)
+        self.head.nxt = self.tail
+        self.tail.prev = self.head
+        
+    def addNode(self, node):
+        first = self.head.nxt 
+        first.prev = node
+        node.prev = self.head
+        node.nxt = first
+        self.head.nxt = node
+        
+        
+    
+    
+    def delNode(self, node ):
+        prev = node.prev
+        nxt = node.nxt
+        nxt.prev = prev
+        prev.nxt = nxt
+        
+        
 
     def get(self, key: int) -> int:
-        if(key in self.cache):
-            self.cache.move_to_end(key)
-            return self.cache[key]
+        if key in self.dictt:
+            node = self.dictt[key]
+            ans = node.val
+            self.delNode(node)
+            self.addNode(node)
+            self.dictt[key] = self.head.nxt
+            return ans
         return -1
-        
         
 
     def put(self, key: int, value: int) -> None:
-        if(key in self.cache):
-            self.cache.move_to_end(key)
-            self.cache[key] = value
+        if key in self.dictt:
+            node = self.dictt[key]
+            del self.dictt[key]
+            self.delNode(node)
         else:
-            if len(self.cache) >= self.cap:
-                self.cache.popitem(last = False)
-            self.cache[key] = value
-            
+            if len(self.dictt) == self.capacity:
+                node = self.tail.prev
+                del self.dictt[node.key]
+                self.delNode(node)
+        self.addNode(self.Node(key,value))
+        self.dictt[key] = self.head.nxt
         
 
 
