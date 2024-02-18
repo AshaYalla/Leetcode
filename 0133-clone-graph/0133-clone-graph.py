f3d1c47b0865af21f082d391d96bc77688dc6b60
Asha Yalla <asha18.yalla@gmail.com>
2024-02-18 13:31:15 -0800

    Time: 31 ms (97.07%), Space: 17.1 MB (50.71%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -9,20 +9,79 @@ class Node:
 class Solution:
     
      def cloneGraph(self, node: 'Node') -> 'Node':
-
-        visited = {}
-        def dfs(n):
-
-            if n in visited:
-                return visited[n]
-            clone = Node(n.val)
-            visited[n] = clone
-
-            for nei in n.neighbors:
-                clone.neighbors.append(dfs(nei))
-            return clone
-        return dfs(node) if node else None
+            if not node:
+                return node
+            dictt = {}
+            qu = [node]
+            visited = set()
+            while qu:
+                n = qu.pop(0)
+                if n.val not in visited:
             
+                    if n not in dictt:
+
+                        dictt[n] = Node(n.val,[])
+                    for i in n.neighbors:
+                        if i not in dictt:
+                            dictt[i] = Node(i.val,[])
+                            qu.append(i)
+                        dictt[n].neighbors.append(dictt[i])
+                    visited.add(n.val)
+            return dictt[node]
+                
+    
+                    
+                
+                
+                
+                    
+                    
+                
+                
+            
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+#         visited = {}
+#         def dfs(n):
+#             if n in visited:
+#                 return visited[n]
+#             clone = Node(n.val)
+#             visited[n] = clone
+
+#             for nei in n.neighbors:
+#                 clone.neighbors.append(dfs(nei))
+#             return clone
+#         return dfs(node) if node else None
+            
+    # def cloneGraph(self, node: 'Node') -> 'Node':
+    # if not node:
+    #     return None
+    # visited = {}
+    # visited[node] = Node(node.val,[])
+    # queue = [node]
+    # while(queue):
+    #     first = queue.pop(0)
+    #     for n in first.neighbors:
+    #         if n not in visited:
+    #             queue.append(n)
+    #             visited[n] = Node(n.val,[])   
+    #         visited[first].neighbors.append(visited[n])
+    # return(visited[node])
 
 
                     
