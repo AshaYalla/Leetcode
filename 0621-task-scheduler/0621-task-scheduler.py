215d3c791acc1fc2dbbddc480d59ad30b3c9ddd6
Asha Yalla <asha18.yalla@gmail.com>
2024-02-20 08:15:08 -0800

    Time: 360 ms (81.39%), Space: 17.2 MB (62.86%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -3,6 +3,19 @@ from collections import Counter
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+#         https://leetcode.com/problems/task-scheduler/discuss/2781024/easy-python-method-with-explanation
+
+        if n == 0: return len(tasks)
+        counter = collections.Counter(tasks)
+        maxCount = 0
+        maxValue = max(counter.values())
+        for cha, val in counter.items():
+            if val == maxValue:
+                maxCount += 1
+        return max((n + 1) * (maxValue - 1) + maxCount ,len(tasks))
+    
+    
+    
         time = 0 
         t = Counter(tasks)
         heap = [-i for i in t.values()]
