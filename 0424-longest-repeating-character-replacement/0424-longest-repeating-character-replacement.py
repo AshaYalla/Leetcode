744e46ac1df09d530107d4974ee36dd3c555844b
Asha Yalla <asha18.yalla@gmail.com>
2024-02-19 16:16:52 -0800

    Time: 102 ms (51.03%), Space: 16.5 MB (94.87%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,21 +1,27 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l = r = 0
-        dictt = {}
-        maxx = 0
-        while(r <= len(s) -1):
-            if(s[r] in dictt):
-                dictt[s[r]]+=1
+        
+        l = 0
+        c_frequency = {}
+        longest_str_len = 0
+        for r in range(len(s)):
+            
+            if not s[r] in c_frequency:
+                c_frequency[s[r]] = 0
+            c_frequency[s[r]] += 1
+            
+            # Replacements cost = cells count between left and right - highest frequency
+            cells_count = r - l + 1
+            if cells_count - max(c_frequency.values()) <= k:
+                longest_str_len = max(longest_str_len, cells_count)
+                
             else:
-                dictt[s[r]]=1
-            while(r-l+1-max(dictt.values()) > k):
-                dictt[s[l]]-=1
-                l+=1
-            maxx = max(maxx,r-l+1)
-            r=r+1
-        return maxx
-
-
+                c_frequency[s[l]] -= 1
+                if not c_frequency[s[l]]:
+                    c_frequency.pop(s[l])
+                l += 1
+        
+        return longest_str_len
             
             
             
