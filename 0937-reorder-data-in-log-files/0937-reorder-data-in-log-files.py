a45597ac1d942070942278a436cca4ff115b6644
Asha Yalla <asha18.yalla@gmail.com>
2024-02-28 06:25:25 -0800

    Time: 26 ms (99.69%), Space: 16.7 MB (47.50%) - LeetHub

---
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letter_logs, digit_logs = [], []
+        for log in logs:
+            if log.split()[1].isalpha():
+                letter_logs.append(log)
+            else:
+                digit_logs.append(log)
+        def weirdOrder(log):
+            all_parts = log.split()
+            key, rest = all_parts[0], all_parts[1:]
+            return [' '.join(rest) , key]
+        letter_logs.sort(key=weirdOrder)
+        return letter_logs + digit_logs
+        
+        
+        
