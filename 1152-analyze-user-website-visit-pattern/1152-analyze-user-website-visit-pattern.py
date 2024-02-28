df0952c6c79cdbe46308b48ec5a75b4be8c0b51d
Asha Yalla <asha18.yalla@gmail.com>
2024-02-28 08:10:01 -0800

    Time: 56 ms (36.10%), Space: 18.5 MB (5.50%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -1,9 +1,43 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
-        users = defaultdict(list)
-        for user, time, site in sorted(zip(username, timestamp, website), key = lambda x:(x[0],x[1])):
-            users[user].append(site)
-        patterns = Counter()
-        for user, sites in users.items():
-            patterns.update(Counter(set(combinations(sites, 3))))
-        return max(sorted(patterns), key = lambda x: patterns[x])
+        hmap = collections.defaultdict(list)
+        N = len(username)
+        for i in range(N):
+            hmap[username[i]].append((timestamp[i], website[i]))
+
+        def get_pattern(websites):
+            res = []
+            def dfs(idx, path):
+                if len(path) == 3:
+                    res.append(path[:])
+                    return
+
+                for i in range(idx + 1, len(websites)):
+                    path.append(websites[i][1])
+                    dfs(i, path)
+                    path.pop()
+
+            dfs(-1, [])
+            return res
+
+        pattern_map = {}
+        for websites in hmap.values():
+            websites.sort()
+            memo = set() # to avoid the same pattern getting added multiple times
+            for pattern in get_pattern(websites):
+                k = tuple(pattern)
+                if k not in memo:
+                    pattern_map[k] = pattern_map.get(k, 0) + 1
+                    memo.add(k)
+        
+        return max(sorted(pattern_map), key=pattern_map.get)
+
+# class Solution:
+#     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+#         users = defaultdict(list)
+#         for user, time, site in sorted(zip(username, timestamp, website), key = lambda x:(x[0],x[1])):
+#             users[user].append(site)
+#         patterns = Counter()
+#         for user, sites in users.items():
+#             patterns.update(Counter(set(combinations(sites, 3))))
+#         return max(sorted(patterns), key = lambda x: patterns[x])
