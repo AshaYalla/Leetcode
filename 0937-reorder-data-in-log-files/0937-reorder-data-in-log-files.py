b0277fb1afd355990a55b4c25ac374d69f2509cd
Asha Yalla <asha18.yalla@gmail.com>
2024-02-28 06:30:35 -0800

    Time: 31 ms (97.84%), Space: 16.5 MB (97.17%) - LeetHub

--- 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.py
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.py
@@ -6,11 +6,11 @@ class Solution:
                 letter_logs.append(log)
             else:
                 digit_logs.append(log)
-        def weirdOrder(log):
-            all_parts = log.split()
-            key, rest = all_parts[0], all_parts[1:]
-            return [' '.join(rest) , key]
-        letter_logs.sort(key=weirdOrder)
+        # def weirdOrder(log):
+        #     all_parts = log.split()
+        #     key, rest = all_parts[0], all_parts[1:]
+        #     return [' '.join(rest) , key]
+        letter_logs.sort(key = lambda x: [x.split(' ')[1:],x[0]])
         return letter_logs + digit_logs
         
         
