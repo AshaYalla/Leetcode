bcc81d84ebb943bc901f276c6ee27c96213de16b
Asha Yalla <asha18.yalla@gmail.com>
2024-02-29 06:44:47 -0800

    Time: 171 ms (78.79%), Space: 20.2 MB (86.69%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -3,37 +3,40 @@ class Solution:
         n = len(accounts)
         parent = [i for i in range(n)]
         rank = [0 for i in range(n)]
+        
         def findp(x):
-            if x!=parent[x]:
-                parent[x] = findp(parent[x])
-            return parent[x]
-        
-        
-        for i in range(n):
-            for j in range(i+1,n):
-                if accounts[i][0] == accounts[j][0]:
-                    for k in range(1,len(accounts[j])):
-                        if accounts[j][k] in accounts[i]:
-                            pi = findp(i)
-                            pj = findp(j)
-                            if pi == pj:
-                                rank[pi]+=1
-                                parent[pj] = pi
-                            elif rank[pi] > rank[pj]:
-                                parent[pj] = pi
-                            else:
-                                parent[pi] = pj
-    
-                            
-        for i in range(n):
-            if i != parent[i]:
-                accounts[findp(i)].extend(accounts[i][1:])
-        res = []
-        for i in range(n):
-            if i == parent[i]:
-                res.append([accounts[i][0]] + sorted(set(accounts[i][1:])))
+            while(parent[x]!=x):
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        def union(i,j):
+            parent[findp(i)] = findp(j)
             
+            
+        owner = dict()
+        
+        
+        for i, (_o, *mails) in enumerate(accounts):
+            for mail in mails:
+                if mail in owner:
+                    union(owner[mail], i)
+                owner[mail] = i
+            
+        mergeacc = defaultdict(list)
+        for i in owner:
+            par = findp(owner[i])
+            mergeacc[par].append(i)
+        res = []
+            
+        for i,j in mergeacc.items():
+            res.append([accounts[i][0]]+sorted(j))
         return res
+            
+
+
+                
+
+                
             
     
             
