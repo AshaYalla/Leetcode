c9426c3c225deed1878ad79c704ea9a3556b1366
Asha Yalla <asha18.yalla@gmail.com>
2024-02-29 10:54:57 -0800

    Time: 1686 ms (40.66%), Space: 22.6 MB (74.67%) - LeetHub

--- 0952-largest-component-size-by-common-factor/0952-largest-component-size-by-common-factor.py
+++ 0952-largest-component-size-by-common-factor/0952-largest-component-size-by-common-factor.py
@@ -1,14 +1,15 @@
 class Solution:
     def largestComponentSize(self, nums: List[int]) -> int:
+        parent = [i for i in range(len(nums))]
+        rank = defaultdict(lambda:0)
         
-        def find(node):
-            if parent[node] == -1: return node
-            else:
-                parent[node] = find(parent[node])
-                return parent[node]
-        
-        def union(idx1,idx2):
-            par1,par2 = find(idx1),find(idx2)
+        def findp(x):
+            while(x!=parent[x]):
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        def union(idx1, idx2):
+            par1,par2 = findp(idx1),findp(idx2)
             if par1!=par2:
                 if rank[par1] > rank[par2]:
                     parent[par2] = par1
@@ -18,20 +19,43 @@ class Solution:
                     parent[par2] = par1
                     rank[par1] += 1
         
-        n = len(nums)
-        parent = defaultdict(lambda:-1)
-        rank = defaultdict(lambda:0)
-        for i in range(n):
+        factor_parents = defaultdict(int)
+    
+        
+        for i,num in enumerate(nums):
             limit = int(nums[i]**0.5)
-            for j in range(2,limit+1):
-                if nums[i] % j == 0:
-                    union(nums[i],j)
-                    union(nums[i],nums[i]//j)
-        count = defaultdict(lambda:0)
-        best = -1
-        for num in nums:
-            par = find(num)
-            tmp = count[par] + 1
-            if tmp > best: best = tmp
-            count[par] = tmp
-        return best
+            for j in range(2, limit+1):
+                if num % j == 0:
+                    fac = num//j
+
+                    if fac in factor_parents:
+                        union(factor_parents[fac] , i)
+                    else:
+                        factor_parents[fac] = i
+                    if j in factor_parents:
+                        union(factor_parents[j] , i)
+                    else:
+                        factor_parents[j] = i
+            if num in factor_parents:
+                union(factor_parents[num], i)
+            else:
+                factor_parents[num] = findp(i)
+            
+
+        
+        childcount = defaultdict(int)
+        maxx = -1
+        for i in range(len(nums)):
+            par = findp(i)
+            childcount[par]+=1
+            if childcount[par] > maxx:
+                maxx = childcount[par]
+        print(parent)
+        return maxx
+            
+            
+            
+            
+            
+            
+        
