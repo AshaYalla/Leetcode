666b9a871bbc90e30f639b00a5102dae1c7c49fa
Asha Yalla <asha18.yalla@gmail.com>
2024-02-28 16:37:37 -0800

    Time: 39 ms (97.55%), Space: 16.6 MB (57.22%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,19 +1,55 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans = []
-        candidates.sort()
-        def combi(tar, comb, start):
-            if tar == 0:
-                if comb not in ans:
-                    ans.append(list(comb))
-                return
-            elif tar < 0:
+        def solve(ind,target):
+            if target==0:
+                ans.append(op[:])
                 return 
-            for i in range(start,len(candidates)):
-                if i > start and candidates[i] == candidates[i-1]:
+            for i in range(ind,len(candidates)):
+                if i>ind and candidates[i]==candidates[i-1]:
                     continue
-                comb.append(candidates[i])
-                combi(tar - candidates[i],comb,i+1)
-                comb.pop()
-        combi(target,[],0)
+                if candidates[i]>target:
+                    break
+                op.append(candidates[i])
+                solve(i+1,target-candidates[i])
+                op.pop()
+        candidates.sort()
+        ans=[]
+        op=[]
+        solve(0,target)
         return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # if target %2 != 0:
+        #     return 0
+        # ans = []
+        # candidates.sort()
+        # dp = dict()
+        # def combi(tar, comb, start):
+        #     if tar == 0:
+        #         if comb not in ans:
+        #             ans.append(list(comb))
+        #         return
+        #     if start == len(candidates):
+        #         return
+        #     elif tar < 0:
+        #         return 
+        #     if (tar, comb, start) in dp:
+        #         return dp[(tar, comb, start)]
+        #     comb.append(candidates[start])
+        #     combi(tar - candidates[start],comb,start+1)
+        #     comb.pop()
+        #     combi(tar,comb,start+1)
+        # combi(target,[],0)
+        # return ans
