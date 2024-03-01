e32dd0ccba355176c6de390bb00fa2403ecd7751
Asha Yalla <asha18.yalla@gmail.com>
2024-03-01 07:55:32 -0800

    Time: 382 ms (77.67%), Space: 36.7 MB (69.75%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,33 +1,56 @@
 class Solution:
+
+    def dfs(self, node, adj, visit):
+        visit[node] = True
+        for neighbor in adj[node]:
+            if not visit[neighbor]:
+                self.dfs(neighbor, adj, visit)
+
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        redundency = 0
-        parent = [i for i in range(n)]
-        def findp(x):
-            if x!= parent[x]:
-                parent[x] = findp(parent[x])
-            return parent[x]
-        
-        
-        for i,j in connections:
-            pi = findp(i) 
-            pj = findp(j)
-            if pi == pj:
-                redundency +=1
-            else:
-                parent[pj] = pi
-        
-        count = 0
-        
-                
-        for i in range(len(parent)) :
-            if i == parent[i]:
-                count+=1
-        count -=1
-        
-        
-        if redundency < count:
+        if len(connections) < n - 1:
             return -1
-        return count
+
+        adj = [[] for _ in range(n)]
+        for connection in connections:
+            adj[connection[0]].append(connection[1])
+            adj[connection[1]].append(connection[0])
+
+        number_of_connected_components = 0
+        visit = [False for _ in range(n)]
+        for i in range(n):
+            if not visit[i]:
+                number_of_connected_components += 1
+                self.dfs(i, adj, visit)
+
+        return number_of_connected_components - 1
+        
+        
+        
+#         redundency = 0
+#         parent = [i for i in range(n)]
+#         def findp(x):
+#             if x!= parent[x]:
+#                 parent[x] = findp(parent[x])
+#             return parent[x]
+        
+#         for i,j in connections:
+#             pi = findp(i) 
+#             pj = findp(j)
+#             if pi == pj:
+#                 redundency +=1
+#             else:
+#                 parent[pj] = pi
+        
+#         count = 0
+                
+#         for i in range(len(parent)) :
+#             if i == parent[i]:
+#                 count+=1
+#         count -=1
+        
+#         if redundency < count:
+#             return -1
+#         return count
                 
                 
                 
