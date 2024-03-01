ebf3fa6a544d8500efacfd1334314ef49dd54660
Asha Yalla <asha18.yalla@gmail.com>
2024-03-01 14:54:08 -0800

    Time: 36 ms (59.84%), Space: 16.5 MB (85.91%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,16 +1,52 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         ans = []
-        def combi(tar, comb, start):
-            if tar == 0:
-                if comb not in ans and len(comb) == k:
-                    ans.append(list(comb))
+        
+        def solve(i,path,summ):
+            if len(path) == k:
+                if summ == n:
+                    ans.append(path[:])
                 return
-            elif tar < 0:
-                return 
-            for i in range(start,10):
-                comb.append(i)
-                combi(tar - i,comb,i+1)
-                comb.pop()
-        combi(n,[],1)
+            if summ > n:
+                return
+            for j in range(i,10):
+                path.append(j)
+                solve(j+1,path,summ+j)
+                path.pop()
+        solve(1,[],0)
         return ans
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # ans = []
+        # def combi(tar, comb, start):
+        #     if tar == 0:
+        #         if comb not in ans and len(comb) == k:
+        #             ans.append(list(comb))
+        #         return
+        #     elif tar < 0:
+        #         return 
+        #     for i in range(start,10):
+        #         comb.append(i)
+        #         combi(tar - i,comb,i+1)
+        #         comb.pop()
+        # combi(n,[],1)
+        # return ans
