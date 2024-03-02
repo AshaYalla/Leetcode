fed40791ac2e5bf7c9d0bf27542e10f010bfc745
Asha Yalla <asha18.yalla@gmail.com>
2024-03-02 10:01:27 -0800

    Time: 39 ms (71.18%), Space: 16.9 MB (38.62%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,14 +2,57 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         ans = []
-        def solve(i,path):
+        check = [True]*len(nums)
+        def solve(i,path,check):
             if i == n:
                 ans.append(path[:])
                 return
-            for j in nums:
-                if j not in path:
-                    path.append(j)
-                    solve(i+1,path)
+            for j in range(len(nums)):
+                if check[j] == True:
+                    check[j] = False
+                    path.append(nums[j])
+                    solve(i+1,path,check)
                     path.pop()
-        solve(0,[])
+                    check[j] = True
+        solve(0,[],check)
         return ans
+    
+    
+    
+    
+    # def permute(self, nums: List[int]) -> List[List[int]]:
+    #     n = len(nums)
+    #     ans = []
+    #     def solve(i,path):
+    #         if i == n:
+    #             ans.append(path[:])
+    #             return
+    #         for j in nums:
+    #             if j not in path:
+    #                 path.append(j)
+    #                 solve(i+1,path)
+    #                 path.pop()
+    #     solve(0,[])
+    #     return ans
+    
+    
+    
+    # class Solution:
+    # def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+    #     def solve(ind,target):
+    #         if target==0:
+    #             ans.append(op[:])
+    #             return 
+    #         for i in range(ind,len(candidates)):
+    #             if i>ind and candidates[i]==candidates[i-1]:
+    #                 continue
+    #             if candidates[i]>target:
+    #                 break
+    #             op.append(candidates[i])
+    #             solve(i+1,target-candidates[i])
+    #             op.pop()
+    #     candidates.sort()
+    #     ans=[]
+    #     op=[]
+    #     solve(0,target)
+    #     return ans
