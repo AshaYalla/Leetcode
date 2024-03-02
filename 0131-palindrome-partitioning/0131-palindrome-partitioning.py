e92a59d1f1c314d08226f30d7ac2943d78fa5523
Asha Yalla <asha18.yalla@gmail.com>
2024-03-02 15:03:31 -0800

    Time: 461 ms (73.47%), Space: 35.3 MB (53.18%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,50 +1,27 @@
+
+
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        n = len(s)
         ans = []
-        def palindrome(ind,arr):
-            if(ind == n ):
-                ans.append(arr[:])
+        n = len(s)
+
+        def backtrack(path, start):
+            if start == n:
+                # all the characters are palindromes, add to the answer
+                ans.append(path[:])
                 return
-            for i in range(ind,n):
-                start = ind
-                end = i
-                flag = 1
-                while(start <= end):
-                    if s[start] != s[end]:
-                        flag = 0
-                        break
-                    start+=1
-                    end-=1
-                if flag:
-                    arr.append(s[ind:i+1])
-                    palindrome(i+1,arr)
-                    arr.pop()
-        palindrome(0,[])
+
+            for i in range(start , n):
+                comb = s[start:i+1]
+                if comb == comb[::-1]:
+                    # the current combination is a palindrome, continue exploring
+                    path.append(comb)
+                    backtrack(path, i+1)
+                    path.pop()
+
+        backtrack([], 0)
+
         return ans
-    
-#     class Solution:
-#     def partition(self, s: str) -> List[List[str]]:
-#         ans = []
-#         n = len(s)
-
-#         def backtrack(path, start):
-#             if start == n:
-#                 # all the characters are palindromes, add to the answer
-#                 ans.append(path[:])
-#                 return
-
-#             for i in range(start + 1, n + 1):
-#                 comb = s[start:i]
-#                 if comb == comb[::-1]:
-#                     # the current combination is a palindrome, continue exploring
-#                     path.append(comb)
-#                     backtrack(path, i)
-#                     path.pop()
-        
-#         backtrack([], 0)
-
-#         return ans
 
             
             
