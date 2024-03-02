cf4868be376c8d6d691fffaffd152779d57a0a94
Asha Yalla <asha18.yalla@gmail.com>
2024-03-02 10:18:31 -0800

    Time: 39 ms (71.18%), Space: 16.9 MB (38.62%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,39 +1,61 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        def solve(path, nums):
+            if not nums:
+                ans.append(path)
+                return
+            for i in nums:
+                solve(path+nums[i], nums[:i]+ nums[i+1:])
+            
+        solve([], nums)
+        return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         n = len(nums)
+#         ans = []
+#         check = [True]*len(nums)
+#         def solve(i,path,check):
+#             if i == n:
+#                 ans.append(path[:])
+#                 return
+#             for j in range(len(nums)):
+#                 if check[j] == True:
+#                     check[j] = False
+#                     path.append(nums[j])
+#                     solve(i+1,path,check)
+#                     path.pop()
+#                     check[j] = True
+#         solve(0,[],check)
+#         return ans
+    
+    
+    
+    
+    def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         ans = []
-        check = [True]*len(nums)
-        def solve(i,path,check):
+        def solve(i,path):
             if i == n:
                 ans.append(path[:])
                 return
-            for j in range(len(nums)):
-                if check[j] == True:
-                    check[j] = False
-                    path.append(nums[j])
-                    solve(i+1,path,check)
+            for j in nums:
+                if j not in path:
+                    path.append(j)
+                    solve(i+1,path)
                     path.pop()
-                    check[j] = True
-        solve(0,[],check)
+        solve(0,[])
         return ans
-    
-    
-    
-    
-    # def permute(self, nums: List[int]) -> List[List[int]]:
-    #     n = len(nums)
-    #     ans = []
-    #     def solve(i,path):
-    #         if i == n:
-    #             ans.append(path[:])
-    #             return
-    #         for j in nums:
-    #             if j not in path:
-    #                 path.append(j)
-    #                 solve(i+1,path)
-    #                 path.pop()
-    #     solve(0,[])
-    #     return ans
     
     
     
