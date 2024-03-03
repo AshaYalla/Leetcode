707bb1243a67678a636ac97d739932e9429c680a
Asha Yalla <asha18.yalla@gmail.com>
2024-03-03 07:56:55 -0800

    Time: 46 ms (5.59%), Space: 16.5 MB (93.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,64 +1,63 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [-1] * n
-        def solve(x):
-            if x == n:
-                return 1
-            if x > n:
-                return 0
-            if dp[x] != -1:
-                return dp[x]
+        
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        first_prev = 2
+        sec_prev = 1
+        ans = 3
+        
+        for i in range(3, n+1):
+            ans = sec_prev + first_prev
+            sec_prev = first_prev
+            first_prev = ans
             
-            l = solve(x+1)
-            r = solve(x+2)
-            dp[x] = l+r
-            return l+r
-            
-            
-        return solve(0)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         prev = 1
-#         cur = 1
-#         ans = 1
-        
-#         for i in range(2,n+1):
-#             ans = prev + cur
-#             prev = cur
-#             cur = ans
-            
-#         return ans
-#         dp = [0]*(n+1)
-#         dp[0] = dp[1] = 1
-        
-#         for i in range(2,n+1):
-#             dp[i] = dp[i-1] + dp[i-2]
-#         return dp[n]
+        return ans
+    
 
-
+        for i in range(2,n+1):
+            ans = prev + cur
+            prev = cur
+            cur = ans
+            
+        return ans
+        
+        
+        
+        
+        
+#         dp = [-1] * n
+#         def solve(x):
+#             if x == n:
+#                 return 1
+#             if x > n:
+#                 return 0
+#             if dp[x] != -1:
+#                 return dp[x]
+            
+#             l = solve(x+1)
+#             r = solve(x+2)
+#             dp[x] = l+r
+#             return l+r
+            
+            
+#         return solve(0)
+        
 
         
         
-        # def solve(i):
-        #     if dp[i] != 0:
-        #         return dp[i]
-        #     if i == 0:
-        #         return 1
-        #     if i < 0:
-        #         return 0
-        #     res = solve(i-1) + solve(i-2)
-        #     dp[i] = res
-        #     return res
-        # return solve(n)
         
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
