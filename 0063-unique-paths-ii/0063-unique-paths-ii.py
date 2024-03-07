fcc10566860d6c02cad1c19498f7d0828a10f3e8
Asha Yalla <asha18.yalla@gmail.com>
2024-03-07 12:15:48 -0800

    Time: 41 ms (77.73%), Space: 16.6 MB (92.75%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,7 +1,28 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        
+        if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1] == 1:
+            return 0
             
         m , n = len(obstacleGrid) , len(obstacleGrid[0])
+        
+        dp = [[0 for i in range(n)] for j in range(m)]
+
+        
+        for r in range(m):
+            for c in range(n):
+                if r == 0 and c == 0:
+                    dp[r][c] = 1
+                    continue
+                if obstacleGrid[r][c] == 1:
+                    dp[r][c] = 0
+                else:
+                    dp[r][c] = dp[r-1][c] + dp[r][c-1]
+                
+        return dp[-1][-1]
+        
+        
+        
         dictt = {}
         def solve(r,c):
             if (r,c) in dictt:
@@ -18,3 +39,4 @@ class Solution:
             dictt[(r,c)] = solve(r-1,c) + solve(r,c-1)
             return dictt[(r,c)]
         return solve(m-1,n-1)
+    
