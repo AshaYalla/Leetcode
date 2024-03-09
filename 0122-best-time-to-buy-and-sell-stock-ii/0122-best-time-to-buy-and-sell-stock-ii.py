e21b77ff09f0bb742bd1eef654a80c0bbc7a282d
Asha Yalla <asha18.yalla@gmail.com>
2024-03-08 18:20:24 -0800

    Time: 69 ms (9.09%), Space: 20 MB (5.02%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,19 +1,47 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = dict()
-        profit = 0
-        def solve(i, buy):
-            if (i,buy) in dp:
-                return dp[(i,buy)]
-            if i == len(prices):
-                return 0
-            if buy:
-                profit = max(- prices[i] + solve(i+1,0), solve(i+1,1))
-            else:
-                profit = max( prices[i] + solve(i+1,1), solve(i+1,0))
-            dp[(i,buy)] = profit
-            return profit
-        return solve(0,1)
+
+        n = len(prices)
+        dp = [[-1 for i in range(2)] for i in range(n+1)]
+
+        dp[n][0] = dp[n][1] = 0
+
+        ind = n-1
+        for ind in range(n-1,-1,-1):
+
+            for buy in range(2):
+                if(buy):
+                    profit = max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1])
+
+                else:
+                    profit = max(prices[ind] + dp[ind+1][1], 0 + dp[ind+1][0])
+
+                dp[ind][buy] = profit
+        return dp[0][1]
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         dp = dict()
+#         profit = 0
+#         def solve(i, buy):
+#             if (i,buy) in dp:
+#                 return dp[(i,buy)]
+#             if i == len(prices):
+#                 return 0
+#             if buy:
+#                 profit = max(- prices[i] + solve(i+1,0), solve(i+1,1))
+#             else:
+#                 profit = max( prices[i] + solve(i+1,1), solve(i+1,0))
+#             dp[(i,buy)] = profit
+#             return profit
+#         return solve(0,1)
                 
                 
         
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         maxprofit=0
+#         for i in range(len(prices)-1):
+#             if prices[i+1]>prices[i]:
+#                 maxprofit+=prices[i+1]-prices[i]
+#         return maxprofit
