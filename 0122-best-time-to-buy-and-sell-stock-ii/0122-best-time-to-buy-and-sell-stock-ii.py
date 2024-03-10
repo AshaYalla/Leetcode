a81c546ad1c6ceded1ff5f341642542ffda96583
Asha Yalla <asha18.yalla@gmail.com>
2024-03-10 10:20:13 -0700

    Time: 87 ms (5.20%), Space: 20.6 MB (5.04%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -13,25 +13,25 @@ class Solution:
                 else:
                     profit = max(prices[ind] + dp[ind-1][1], dp[ind-1][0])
                 dp[ind][buy] = profit
-
+        print(dp)
         return dp[n-1][0]
     
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        dp = dict()
-        profit = 0
-        def solve(i, buy):
-            if (i,buy) in dp:
-                return dp[(i,buy)]
-            if i == len(prices):
-                return 0
-            if buy:
-                profit = max(- prices[i] + solve(i+1,0), solve(i+1,1))
-            else:
-                profit = max( prices[i] + solve(i+1,1), solve(i+1,0))
-            dp[(i,buy)] = profit
-            return profit
-        return solve(0,1)
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         dp = dict()
+#         profit = 0
+#         def solve(i, buy):
+#             if (i,buy) in dp:
+#                 return dp[(i,buy)]
+#             if i == len(prices):
+#                 return 0
+#             if buy:
+#                 profit = max(- prices[i] + solve(i+1,0), solve(i+1,1))
+#             else:
+#                 profit = max( prices[i] + solve(i+1,1), solve(i+1,0))
+#             dp[(i,buy)] = profit
+#             return profit
+#         return solve(0,1)
                 
                 
         
