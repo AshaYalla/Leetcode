4fe376091fc496838414ca545bf753e1a62f669b
Asha Yalla <asha18.yalla@gmail.com>
2024-03-10 09:08:20 -0700

    Time: 220 ms (28.76%), Space: 26.5 MB (35.62%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -22,21 +22,21 @@ class Solution:
         
         dp = [[[-1 for _ in range(k + 1)] for _ in range(3)] for _ in range(len(prices) + 1)]
         
-        def recursion_with_memoization(p, ind, can_buy, k, dp):
-            if ind >= len(p) or k <= 0:
+        def recursion_with_memoization(ind, can_buy, k):
+            if ind >= len(prices) or k <= 0:
                 return 0
 
             if dp[ind][can_buy][k] != -1:
                 return dp[ind][can_buy][k]
 
             if can_buy:
-                dp[ind][can_buy][k] = max(-p[ind] + recursion_with_memoization(p, ind + 1, not can_buy, k, dp), recursion_with_memoization(p, ind + 1, can_buy, k, dp))
+                dp[ind][can_buy][k] = max(-prices[ind] + recursion_with_memoization(ind + 1, not can_buy, k), recursion_with_memoization(ind + 1, can_buy, k, ))
             else:
-                dp[ind][can_buy][k] = max(p[ind] + recursion_with_memoization(p, ind + 1, not can_buy, k - 1, dp), recursion_with_memoization(p, ind + 1, can_buy, k, dp))
+                dp[ind][can_buy][k] = max(prices[ind] + recursion_with_memoization(ind + 1, not can_buy, k - 1), recursion_with_memoization( ind + 1, can_buy, k))
 
             return dp[ind][can_buy][k]
         
-        return recursion_with_memoization(prices, 0, True, k, dp)
+        return recursion_with_memoization(0, True, k)
 
     
 
