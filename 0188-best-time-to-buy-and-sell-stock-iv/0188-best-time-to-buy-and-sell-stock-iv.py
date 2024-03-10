5b7ffe5b6d254f89fed98ce77f219a0ea7259880
Asha Yalla <asha18.yalla@gmail.com>
2024-03-10 08:58:39 -0700

    Time: 198 ms (31.07%), Space: 22.3 MB (42.32%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,40 @@
+# class Solution:
+#     def maxProfit(self, k: int, prices: List[int]) -> int:
+#         dp = [[[-1 for _ in range(k + 1)] for _ in range(3)] for _ in range(len(prices) + 1)]
+#         profit = 0
+#         def solve(i, buy, cap):
+#             if i >= len(prices) or cap <= 0:
+#                 return 0
+#             if dp[i][buy][k] != -1:
+#                 return dp[i][buy][k]
+
+#             if buy:
+#                 profit = max(- prices[i] + solve(i+1,0,cap), solve(i+1,1,cap))
+#             else:
+#                 profit = max( prices[i] + solve(i+1,1,cap -1), solve(i+1,0,cap))
+#             dp[i][buy][k] = profit
+#             return dp[i][buy][k]
+#         return solve(0,1,k)
+    
+    
+class Solution:
+    def recursion_with_memoization(self, p, ind, can_buy, k, dp):
+        if ind >= len(p) or k <= 0:
+            return 0
+
+        if dp[ind][can_buy][k] != -1:
+            return dp[ind][can_buy][k]
+
+        if can_buy:
+            dp[ind][can_buy][k] = max(-p[ind] + self.recursion_with_memoization(p, ind + 1, not can_buy, k, dp), self.recursion_with_memoization(p, ind + 1, can_buy, k, dp))
+        else:
+            dp[ind][can_buy][k] = max(p[ind] + self.recursion_with_memoization(p, ind + 1, not can_buy, k - 1, dp), self.recursion_with_memoization(p, ind + 1, can_buy, k, dp))
+
+        return dp[ind][can_buy][k]
+
+    def maxProfit(self, k, prices):
+        dp = [[[-1 for _ in range(k + 1)] for _ in range(3)] for _ in range(len(prices) + 1)]
+        return self.recursion_with_memoization(prices, 0, True, k, dp)
+
+    
+
