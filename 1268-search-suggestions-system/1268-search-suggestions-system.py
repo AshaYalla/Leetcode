ed32fbfd2cdb909f341abeefffe5637e6055d54b
Asha Yalla <asha18.yalla@gmail.com>
2024-03-14 06:39:50 -0700

    Time: 74 ms (75.63%), Space: 19.7 MB (82.29%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -9,10 +9,11 @@ class Solution:
             tmp = []
             input_char += chr
             insertion_index = self.binary_search(products, input_char)
-            for word_ind in range(insertion_index, min(array_len, insertion_index+3)):
+            for word_ind in range(insertion_index, min(len(products), insertion_index+3)):
                 if products[word_ind].startswith(input_char):
                     tmp.append(products[word_ind])
             ans.append(tmp)
+            products = products[insertion_index:]
         return ans
 
     def binary_search(self, array, target):
