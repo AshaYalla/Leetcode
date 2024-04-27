9b522c06283dc673820cd30ce1d7f77d572c1007
Asha Yalla <asha18.yalla@gmail.com>
2024-04-26 18:01:00 -0700

    Time: 96 ms (33.28%), Space: 20.7 MB (39.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
-        ans = collections.defaultdict(list)
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            ans[tuple(count)].append(s)
-        return ans.values()
-            
+        dictt = defaultdict(list)
         
+        for word in strs:
+            dictt[tuple(sorted(word))].append(word)
+        return dictt.values()
+    
+#dictt keys should be hashable cant use set or list
+# tuple values cant be changed is immutable hence hashable 
+#frozenset is also hashable but cant be used as set removes duplciates
