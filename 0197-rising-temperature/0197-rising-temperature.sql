1364a98d854e1d642ddbe44ed19a7d6183b44c96
Asha Yalla <asha18.yalla@gmail.com>
2024-05-06 15:12:43 -0700

    Time: 687 ms (90.36%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -5,7 +5,14 @@
 # on datediff(w1.recordDate, w2.recordDate) = 1
 # where   w1.temperature > w2.temperature 
 
-select id from (
-    select *, lag(temperature,1) over(order by recordDate) as prevtemp, lag(recordDate,1) over(order by recordDate) as prevdate
-from weather) a
-where datediff(recordDate, prevdate) = 1 and temperature > prevtemp
+select id from
+(select *, lag(recordDate, 1) over(order by recordDate asc) as prevdate,
+lag(temperature,1 ) over(order by recordDate asc) as prevtemp
+from weather)A 
+where A.temperature > A.prevtemp and datediff(recordDate, prevdate) = 1
+
+
+# select id from (
+#     select *, lag(temperature,1) over(order by recordDate) as prevtemp, lag(recordDate,1) over(order by recordDate) as prevdate
+# from weather) a
+# where datediff(recordDate, prevdate) = 1 and temperature > prevtemp
