43ef64d248aefd285be18d2e7634ab1ceaae516f
Asha Yalla <asha18.yalla@gmail.com>
2024-05-07 08:45:54 -0700

    Time: 1920 ms (36.27%), Space: 0B (100.00%) - LeetHub

---
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,6 @@
+/* Write your T-SQL query statement below */
+
+select actor_id, director_id from (select distinct actor_id, director_id, count(*) as meetcount
+from ActorDirector
+group by actor_id, director_id) coord
+where coord.meetcount >= 3
