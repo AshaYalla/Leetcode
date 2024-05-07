dcef9509c0259f67990b4c2b0e41a4c645ccdf1d
Asha Yalla <asha18.yalla@gmail.com>
2024-05-07 11:56:11 -0700

    Time: 1593 ms (86.08%), Space: 0B (100.00%) - LeetHub

--- 1083-sales-analysis-ii/1083-sales-analysis-ii.sql
+++ 1083-sales-analysis-ii/1083-sales-analysis-ii.sql
@@ -1,6 +1,3 @@
-/* Write your T-SQL query statement below */
-
-
 SELECT DISTINCT s.buyer_id
 FROM Sales s
 JOIN Product p
@@ -16,18 +13,3 @@ LEFT JOIN
     )a
 ON s.buyer_id = a.buyer_id
 WHERE a.buyer_id IS NULL
-
--- select distinct buyer_id from sales s
--- left join 
--- product p 
--- ON p.product_id = s.product_id
--- where p.product_name = 'S8'
--- and buyer_id not in (
-    
---     SELECT DISTINCT buyer_id
--- FROM Sales s
--- JOIN Product p
--- ON s.product_id = p.product_id
--- where p.product_name = 'iPhone'
-
--- )
