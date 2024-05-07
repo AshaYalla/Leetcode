a61038c4a37a5abc4fd02f1a846be8218dc0d172
Asha Yalla <asha18.yalla@gmail.com>
2024-05-07 08:46:43 -0700

    Time: 1946 ms (34.48%), Space: 0B (100.00%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,6 +1,4 @@
-/* Write your T-SQL query statement below */
-
-select actor_id, director_id from (select distinct actor_id, director_id, count(timestamp) as meetcount
-from ActorDirector
-group by actor_id, director_id) coord
-where coord.meetcount >= 3
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(timestamp) >= 3;
