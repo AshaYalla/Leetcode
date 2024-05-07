f56e069262ed8b009e3ec248a05a0e9acadab3b5
Asha Yalla <asha18.yalla@gmail.com>
2024-05-07 11:36:26 -0700

    Time: 5676 ms (17.54%), Space: 0B (100.00%) - LeetHub

--- 1082-sales-analysis-i/1082-sales-analysis-i.sql
+++ 1082-sales-analysis-i/1082-sales-analysis-i.sql
@@ -1,4 +1,8 @@
-/* Write your T-SQL query statement below */
+-- SELECT seller_id FROM (
+--     SELECT seller_id, RANK() OVER (ORDER BY SUM(price) DESC) as rk 
+--     FROM Sales GROUP BY seller_id
+-- )q1 WHERE q1.rk=1;
+
 with cte_sales as(
 select seller_id, sum(price) as total_sales
 from sales 
@@ -7,4 +11,3 @@ group by seller_id
 
 select seller_id from cte_sales 
 where total_sales = (select max(total_sales) from cte_sales)
-
