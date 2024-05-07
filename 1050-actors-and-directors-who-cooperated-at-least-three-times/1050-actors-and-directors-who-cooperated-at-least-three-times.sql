b18733d59652e88947143c5b42107580d1319bfd
Asha Yalla <asha18.yalla@gmail.com>
2024-05-07 08:52:49 -0700

    Time: 1994 ms (30.72%), Space: 0B (100.00%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,4 +1,10 @@
-SELECT actor_id, director_id
-FROM ActorDirector
-GROUP BY actor_id, director_id
-HAVING COUNT(timestamp) >= 3;
+with cte_meet as (
+    select actor_id, director_id,
+    row_number() over (partition by actor_id, director_id order by timestamp ) rt
+    from ActorDirector
+    
+)
+
+select distinct actor_id, director_id from cte_meet
+where rt>=3
+
