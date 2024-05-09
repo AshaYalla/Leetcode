8a37d717415a3b14e77b685f542bc931bd9d0311
Asha Yalla <asha18.yalla@gmail.com>
2024-05-09 12:22:24 -0700

    Time: 993 ms (45.22%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,3 +1,9 @@
 # Write your MySQL query statement below
-select player_id, min(event_date) as first_login  from activity 
-group by player_id
+# select player_id, min(event_date) as first_login  from activity 
+# group by player_id
+
+
+select player_id, event_date as first_login from 
+(select player_id, event_date, row_number() over(PARTITION BY player_id ORDER BY event_date asc) loginorder
+from activity) l
+where loginorder = 1
