5901a1603f0423502a69ca171bdc7f76c8d0078a
Asha Yalla <asha18.yalla@gmail.com>
2024-05-09 11:54:20 -0700

    Time: 876 ms (67.44%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,4 +1,3 @@
-/* Write your T-SQL query statement below */
-
-select player_id, min(event_date) as first_login from Activity
+# Write your MySQL query statement below
+select player_id, min(event_date) as first_login  from activity 
 group by player_id
