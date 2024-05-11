ade5e82decdb8e991bf454e48969086fcebf2093
Asha Yalla <asha18.yalla@gmail.com>
2024-05-11 07:23:48 -0700

    Time: 1281 ms (28.85%), Space: 0B (100.00%) - LeetHub

---
+++ 1777-products-price-for-each-store/1777-products-price-for-each-store.sql
@@ -0,0 +1,18 @@
+/* Write your T-SQL query statement below */
+
+select product_id, store1, store2, store3 from Products
+pivot(
+max(price) for store in ([store1], [store2], [store3])
+
+) as p
+
+
+-- select
+--     product_id,
+--     store1,
+--     store2,
+--     store3
+-- from Products
+-- pivot (
+--     sum(price) for store in (store1,store2,store3)
+-- ) as p
