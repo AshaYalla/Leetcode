5c8eadf64bf88b640313e0baa630a586db1d9d02
Asha Yalla <asha18.yalla@gmail.com>
2024-05-11 10:50:24 -0700

    Time: 2549 ms (28.61%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,5 +1,17 @@
 /* Write your T-SQL query statement below */
-SELECT user_id, MAX(time_stamp) AS last_stamp
-FROM Logins
-WHERE YEAR(time_stamp) = 2020
-GROUP BY user_id
+-- SELECT user_id, MAX(time_stamp) AS last_stamp
+-- FROM Logins
+-- WHERE YEAR(time_stamp) = 2020
+-- GROUP BY user_id
+
+select
+    user_id, time_stamp as last_stamp
+from (
+    select 
+        *,
+        r = row_number() over (partition by user_id order by time_stamp desc) 
+    from 
+        logins 
+    where year(time_stamp) = 2020
+) sub
+where sub.r = 1
