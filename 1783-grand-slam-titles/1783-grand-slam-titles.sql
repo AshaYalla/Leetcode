8eab0cc81dbf9f549440e07b15b46df66c69b522
Asha Yalla <asha18.yalla@gmail.com>
2024-05-24 17:39:33 -0700

    Time: 1310 ms (81.92%), Space: 0B (100.00%) - LeetHub

---
+++ 1783-grand-slam-titles/1783-grand-slam-titles.sql
@@ -0,0 +1,10 @@
+SELECT player_id,player_name,
+SUM(case when player_id=Wimbledon then 1 else 0 end) +
+SUM(case when player_id=Fr_open then 1 else 0 end) +
+SUM(case when player_id=US_open then 1 else 0 end) +
+SUM(case when player_id=Au_open then 1 else 0 end)
+as grand_slams_count
+FROM Players
+JOIN Championships
+ON player_id IN (Wimbledon, Fr_open, US_open , Au_open) 
+GROUP BY player_id,player_name;
