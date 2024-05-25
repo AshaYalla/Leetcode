f08cf53c631e1ea3fcc5cc7384d153b0e2b0a703
Asha Yalla <asha18.yalla@gmail.com>
2024-05-24 17:40:41 -0700

    Time: 1509 ms (56.38%), Space: 0B (100.00%) - LeetHub

--- 1783-grand-slam-titles/1783-grand-slam-titles.sql
+++ 1783-grand-slam-titles/1783-grand-slam-titles.sql
@@ -6,5 +6,8 @@ SUM(case when player_id=Au_open then 1 else 0 end)
 as grand_slams_count
 FROM Players
 JOIN Championships
-ON player_id IN (Wimbledon, Fr_open, US_open , Au_open) 
+ON player_id  = Wimbledon
+or player_id  = Fr_open 
+or player_id  = US_open 
+or player_id  = Au_open
 GROUP BY player_id,player_name;
