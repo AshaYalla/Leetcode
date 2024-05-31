6b022f549c33e9946db764b351b510c7fd208c16
Asha Yalla <asha18.yalla@gmail.com>
2024-05-31 12:38:42 -0700

    Time: 781 ms (82.50%), Space: 0B (100.00%) - LeetHub

--- 0615-average-salary-departments-vs-company/0615-average-salary-departments-vs-company.sql
+++ 0615-average-salary-departments-vs-company/0615-average-salary-departments-vs-company.sql
@@ -6,9 +6,10 @@ SELECT DISTINCT pay_month, department_id,
     WHEN avg_dept_sal = avg_company_sal THEN 'same'
     END
 ) as comparison
+
 FROM 
 (
-    SELECT s.employee_id, amount, pay_Date, department_id, LEFT(s.pay_date, 7) as pay_month,
+    SELECT distinct department_id, LEFT(s.pay_date, 7) as pay_month,
      AVG(s.amount) OVER(PARTITION BY s.pay_date) as avg_company_sal,
      AVG(s.amount) OVER(PARTITION BY s.pay_Date, e.department_id) as avg_dept_sal
     FROM Salary s
