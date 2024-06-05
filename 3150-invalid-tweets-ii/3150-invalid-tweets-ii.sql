c7a79d8013e10fbfb498eddc1eea2b5ace115791
Asha Yalla <asha18.yalla@gmail.com>
2024-06-05 05:50:00 -0700

    Time: 276 ms (100.00%), Space: 0B (100.00%) - LeetHub

---
+++ 3150-invalid-tweets-ii/3150-invalid-tweets-ii.sql
@@ -0,0 +1,6 @@
+SELECT tweet_id
+FROM Tweets
+WHERE LEN(content) > 140
+   OR (LEN(content) - LEN(REPLACE(content, '@', ''))) > 3
+   OR (LEN(content) - LEN(REPLACE(content, '#', ''))) > 3
+ORDER BY tweet_id ASC;
