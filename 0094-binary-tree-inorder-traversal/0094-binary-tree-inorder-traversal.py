9bb9da76411a2665d91a27e22dfcb3ff3a15f588
Asha Yalla <asha18.yalla@gmail.com>
2024-08-08 10:20:56 -0500

    Time: 35 ms (56.61%), Space: 16.5 MB (15.82%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -104,36 +104,3 @@ class Solution:
 #         return ans
 
 
-# class Solution:
-#     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-#         res = []
-        
-#         def dfs(x):
-#             if x:
-#                 dfs(x.left)
-#                 res.append(x.val)
-#                 dfs(x.right)
-                
-                
-            
-            
-#         dfs(root)
-#         return res
-            
-#         res = []
-#         stack = []
-#         node = root
-        
-#         while(stack or node):
-#             while(node):
-#                 stack.append(node)
-#                 node = node.left
-#             ele = stack.pop()
-#             res.append(ele.val)
-#             node = ele.right
-#         return res
-            
-                
-
-
-        
