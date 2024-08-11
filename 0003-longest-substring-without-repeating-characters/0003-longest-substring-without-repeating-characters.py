5a1115b6c1c8e741670965089270aef5d5b50599
Asha Yalla <asha18.yalla@gmail.com>
2024-08-10 20:16:18 -0500

    Time: 48 ms (86.94%), Space: 16.6 MB (35.69%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,38 +1,13 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        sett = set()
-        start = 0
-        maxx = 0
-        for i in range(len(s)):
-            while s[i] in sett:
-                sett.remove(s[start])
-                start+=1
-            sett.add(s[i])
-            if(len(sett) == i-start+1):
-                maxx = max(maxx,i-start+1)
-        return maxx
-        dict = {}
-        res = 0
-        
+        dictt = defaultdict(int)
         l = 0
+        res = 0
         for r in range(len(s)):
-            if s[r] in dict:
-                l = max(l, dict[s[r]] + 1)
+            if s[r] in dictt:
+                l = max(l,dictt[s[r]]+1)
+            dictt[s[r]] = r
             res = max(res, r-l+1)
-            dict[s[r]] = r
-            
         return res
-
-    # def lengthOfLongestSubstring(self, s: str) -> int:
-    #     sett = set()
-    #     start = 0
-    #     maxx = 0
-    #     for i in range(len(s)):
-    #         while s[i] in sett:
-    #             sett.remove(s[start])
-    #             start+=1
-    #         sett.add(s[i])
-    #         if(len(sett) == i-start+1):
-    #             maxx = max(maxx,i-start+1)
-    #     return maxx
-        
+                
+                
