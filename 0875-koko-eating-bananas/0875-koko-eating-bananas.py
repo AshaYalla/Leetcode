e7c702e8d249cadd815b90661727df3463687a05
Asha Yalla <asha18.yalla@gmail.com>
2024-08-10 23:47:17 -0500

    Time: 260 ms (72.62%), Space: 18.2 MB (46.34%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -18,6 +18,22 @@ class Solution:
                 
         return l
                 
-
-                
+        l = min(piles)
+        r = max(piles)
         
+        while(l <r):
+            mid = l + r // 2
+            hoursreq = 0
+            for i in piles:
+                hoursreq += i // mid
+                if i % mid:
+                    hoursreq+=1
+            
+            if hoursreq <= h:
+                r = mid
+                
+            else:
+                l = mid + 1
+                
+        return l
+                  
